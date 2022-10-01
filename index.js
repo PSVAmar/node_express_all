f24fe8bb245759d9fbde164af2aa5089040b0110
Amar Pulavarthi <20pa1a05d4@vishnu.edu.in>
2022-10-01 10:26:07 +0530

    Added express to application

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("HEllo World");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! weuigfbsjg')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
